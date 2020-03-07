ea837ef6bb881643483c4a8f89024c5264da9332
Tony <tony2door@yahoo.com>
2020-03-07 11:36:26 +0200

    shopping list exercise

---
+++ .idea/CumparaturiJS.js
@@ -0,0 +1,27 @@
+Vue.component('cumparaturi-component',{
+    props: ['item'],
+    template: '<tr><td>{{item.nume}}</td><td>{{item.cant}}</td></tr>'
+})
+
+var app = new Vue({
+    el: '#shoppinglist',
+    data: {
+        produs:{
+            numeProdus: '',
+            cantitate: ''
+        },
+        produse:[]
+    },
+
+    methods : {
+        addProdus: function () {
+            this.produse.push({
+                nume: this.produs.numeProdus,
+                cant: this.produs.cantitate
+            });
+            this.produs.numeProdus = '';
+            this.produs.cantitate = '',
+                console.log('se vede motzu')
+        }
+    }
+})
